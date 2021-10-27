737a812489fef4871a1d9c5b4f2b27b14961714e
vin <beingvini@gmail.com>
2021-10-27 18:56:11 +0530

    create about page and social media links.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
+console.log("javascript linked")
 
+let mouseCurser = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll('.header ul li')
+
+window.addEventListener('mousemove', (e) => {
+  mouseCurser.style.top = e.pageY + 'px';
+  mouseCurser.style.left = e.pageX + 'px';
+  console.log(e);
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("mouseleave",()=>{
+    mouseCurser.classList.remove('link-bubble')
+  });
+  link.addEventListener("mouseover",()=>{
+    mouseCurser.classList.add('link-bubble')
+  });
+});
