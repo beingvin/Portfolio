c215590c0030142d7450cb54208db3be620de36f
beingvin <beingvini@gmail.com>
2022-04-27 12:46:16 +0530

    update 3.1.  make responsive navabar & UI , show skills bar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 
 console.log("javascript linked")
 
+/////////////////////////////////////// toggle navigation
+
+let toggle = document.getElementById("toggle")
+let menu = document.querySelector(".navBar ul")
+
+toggle.onclick = ()=>{
+    toggle.classList.toggle("active");
+    menu.classList.toggle("active");
+}
+
+/////////////////////////////////////// change cursor format 
+
 let loader = document.getElementById('preloader');
 window.addEventListener('load', () => {
   loader.style.display = "none";
@@ -25,3 +37,61 @@ navLinks.forEach((link) => {
   });
 });
 
+
+
+
+/////////////////////////////////////// Skills 
+
+// create variables
+let html = document.querySelector(".html span");
+let css = document.querySelector(".css span");
+let php = document.querySelector(".php span");
+let javascript = document.querySelector(".javascript span");
+let python = document.querySelector(".python span");
+let react = document.querySelector(".react span");
+
+// crate keyFrames
+let keyFrames = document.createElement("style");
+
+keyFrames.innerHTML = `
+@keyframes html {
+    100% {
+        width: ${html.innerHTML};
+    }
+}
+@keyframes css {
+    100% {
+        width: ${css.innerHTML};
+    }
+}
+@keyframes php {
+    100% {
+        width: ${php.innerHTML};
+    }
+}
+@keyframes javascript {
+    100% {
+        width: ${javascript.innerHTML};
+    }
+}
+@keyframes python {
+    100% {
+        width: ${python.innerHTML};
+    }
+}
+@keyframes react {
+    100% {
+        width: ${react.innerHTML};
+    }
+}
+
+`;
+
+// append keyFrames
+
+html.appendChild(keyFrames);
+css.appendChild(keyFrames);
+php.appendChild(keyFrames);
+javascript.appendChild(keyFrames);
+python.appendChild(keyFrames);
+react.appendChild(keyFrames);
